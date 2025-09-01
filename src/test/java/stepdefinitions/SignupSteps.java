9417104360cf202890f2d97bc09386bef1644414
nagaraju <nagaraju.qatrainer@gmail.com>
2025-09-01 10:38:17 +0530

    New changes commit

--- src/test/java/stepdefinitions/SignupSteps.java
+++ src/test/java/stepdefinitions/SignupSteps.java
@@ -1,5 +1,17 @@
 package stepdefinitions;
 
+
+import org.testng.annotations.Test;
+
 public class SignupSteps {
+	
+	
+	
+	@Test
+	public void jenkinsTest() {
+		
+		System.out.println("This is jenkins test");
+		
+	}
 
 }
